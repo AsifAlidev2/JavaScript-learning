36f6350ed2a616d8433dbc83baea708056fbcbcf
AsifAlidev2 <asifrodnani111@gmail.com>
2026-01-07 00:57:16 +0500

    Initial commit with folder structure

---
+++ 01-Variables/script.js
@@ -0,0 +1,9 @@
+// Variables & Data Types
+
+let age = 22;
+const name = "Asif";
+let isStudent = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Student:", isStudent);
---
+++ 02-Operators/script.js
@@ -0,0 +1,21 @@
+// Operators are used to perform some operations
+
+let a = 8;
+let b = 5;
+
+
+// + used for addition
+console.log("Addition:", a + b);       // Addition: 13
+
+// - used for subtraction
+console.log("Subtraction:", a - b);    // Subtraction: 3
+
+// * used for Multiplication
+console.log("Multiplication:", a * b); // Multiplication: 40
+
+// / used for Division 
+console.log("Division:", a / b);       // Division: 1.6
+
+// % used to find remainder
+
+console.log("Remainder:", a % b);      // Remainder: 3
